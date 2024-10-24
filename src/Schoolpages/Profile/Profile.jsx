229c7c0d753d3e4fefd0850950418b240abed36a
sandipto729 <sandipto729@gmail.com>
2024-10-24 16:37:26 +0530

    Correct the user fetch problem

--- src/Schoolpages/Profile/Profile.jsx
+++ src/Schoolpages/Profile/Profile.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import UserProfile from "./UserProfile/UserProfile";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
-    
-
+    const user = useSelector(state => state?.user?.user);
     return (
         <div>
-            <UserProfile/>
+            <UserProfile user={user}/>
         </div>
     );
 };
