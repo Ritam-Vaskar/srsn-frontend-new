fd6049eecf265a3c2a261b730008c30ebb1910be
sandipto729 <sandipto729@gmail.com>
2024-12-10 11:44:54 +0530

    change class fees qr

--- src/helper/classFees.js
+++ src/helper/classFees.js
@@ -6,10 +6,11 @@ const classFees = {
     2: { amount: 2000, qrCode: 'https://ibb.co/2qtc0Lx' ,qrCodeImg:"https://i.ibb.co/mJqhrYk/upiQR.png"},
     3: { amount: 2000, qrCode: 'https://ibb.co/2qtc0Lx' ,qrCodeImg:"https://i.ibb.co/mJqhrYk/upiQR.png"},
     4: { amount: 2000, qrCode: 'https://ibb.co/2qtc0Lx',qrCodeImg:"https://i.ibb.co/mJqhrYk/upiQR.png" },
-    5: { amount: 0, qrCode: 'https://res.cloudinary.com/duoxuw9xh/image/upload/v1733803733/tpdsolsvdr2bjtioo8rn.webp'},
-    6: { amount: 0, qrCode: 'https://res.cloudinary.com/duoxuw9xh/image/upload/v1733803733/tpdsolsvdr2bjtioo8rn.webp' },
-    7: { amount: 0, qrCode: 'https://res.cloudinary.com/duoxuw9xh/image/upload/v1733803733/tpdsolsvdr2bjtioo8rn.webp' },
-    8: { amount: 0, qrCode: 'https://res.cloudinary.com/duoxuw9xh/image/upload/v1733803733/tpdsolsvdr2bjtioo8rn.webp' },
+
+    5: { amount: 0,qrCode:'https://res.cloudinary.com/duoxuw9xh/image/upload/v1733803733/tpdsolsvdr2bjtioo8rn.webp', qrCodeImg: 'https://res.cloudinary.com/duoxuw9xh/image/upload/v1733803733/tpdsolsvdr2bjtioo8rn.webp'},
+    6: { amount: 0,qrCode:'https://res.cloudinary.com/duoxuw9xh/image/upload/v1733803733/tpdsolsvdr2bjtioo8rn.webp',qrCodeImg: 'https://res.cloudinary.com/duoxuw9xh/image/upload/v1733803733/tpdsolsvdr2bjtioo8rn.webp' },
+    7: { amount: 0, qrCode:'https://res.cloudinary.com/duoxuw9xh/image/upload/v1733803733/tpdsolsvdr2bjtioo8rn.webp',qrCodeImg: 'https://res.cloudinary.com/duoxuw9xh/image/upload/v1733803733/tpdsolsvdr2bjtioo8rn.webp' },
+    8: { amount: 0,qrCode:'https://res.cloudinary.com/duoxuw9xh/image/upload/v1733803733/tpdsolsvdr2bjtioo8rn.webp', qrCodeImg: 'https://res.cloudinary.com/duoxuw9xh/image/upload/v1733803733/tpdsolsvdr2bjtioo8rn.webp' },
   };
   
   export default classFees;
