20eecde8da23c5c027f052ecfb6dc8cf8146b7ea
Ritam Vaskar <vaskarananda77@gmail.com>
2025-02-07 11:54:19 +0530

    Update HeroCarousel.jsx

--- src/sections/HeroCarousel/HeroCarousel.jsx
+++ src/sections/HeroCarousel/HeroCarousel.jsx
@@ -4,7 +4,6 @@ import styles from './styles/HeroCarousel.module.scss'; // Import your custom st
 
 const HeroCarousel = () => {
   const images = [
-    "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/679dead913371e4a4ee6c86a_WhatsApp%20Image%202025-01-31%20at%2021.37.18_3bbfb479.jpg",
     "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/6713bd34e03c2b125c749e9c_28071260_1907983642553541_4952021693143437781_o.jpg",
     "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/6713bd34be2521c91c45aefb_301128514_562327412349985_3679940258914638492_n.jpg",
     "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/6713bd3407219700795ae005_49769298_2354597557892145_2033352142013595648_n.jpg"
