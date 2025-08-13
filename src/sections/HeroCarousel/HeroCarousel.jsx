0e060c5b9e68d78c2eaa4e5ee3625e88e5ff2945
sandipto729 <sandipto729@gmail.com>
2025-08-13 09:41:07 +0530

    fix: correct image order in HeroCarousel component

--- src/sections/HeroCarousel/HeroCarousel.jsx
+++ src/sections/HeroCarousel/HeroCarousel.jsx
@@ -7,7 +7,8 @@ const HeroCarousel = () => {
     "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/68124b6c40177c9489653fa7_IMG-20250430-WA0005.jpg", 
     "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/6713bd34e03c2b125c749e9c_28071260_1907983642553541_4952021693143437781_o.jpg",
     "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/6713bd34be2521c91c45aefb_301128514_562327412349985_3679940258914638492_n.jpg",
-    "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/6713bd3407219700795ae005_49769298_2354597557892145_2033352142013595648_n.jpg"
+    "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/6713bd3407219700795ae005_49769298_2354597557892145_2033352142013595648_n.jpg",
+    "https://res.cloudinary.com/dodpgohuc/image/upload/v1755058168/7c27f319-d2ae-419b-a9df-a719018e8fe7_na2kbw.jpg"
   ];
 
   const settings = {
