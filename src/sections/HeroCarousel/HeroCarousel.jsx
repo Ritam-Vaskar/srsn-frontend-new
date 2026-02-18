441e7497a52db4b74f62598f06441da152ea6ae7
Ritam Vaskar <vaskarananda77@gmail.com>
2026-02-18 20:23:40 +0530

    Add two new image URLs to HeroCarousel

--- src/sections/HeroCarousel/HeroCarousel.jsx
+++ src/sections/HeroCarousel/HeroCarousel.jsx
@@ -8,6 +8,8 @@ const HeroCarousel = () => {
     "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/6713bd34e03c2b125c749e9c_28071260_1907983642553541_4952021693143437781_o.jpg",
     "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/6713bd34be2521c91c45aefb_301128514_562327412349985_3679940258914638492_n.jpg",
     "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/6713bd3407219700795ae005_49769298_2354597557892145_2033352142013595648_n.jpg",
+    "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/6995d210095da56f5172d58c_WhatsApp%20Image%202026-02-18%20at%208.17.23%20PM.jpeg",
+    "https://cdn.prod.website-files.com/663d1907e337de23e83c30b2/6995d2109d07d21b6b988f94_WhatsApp%20Image%202026-02-18%20at%208.17.24%20PM.jpeg"
     // "https://res.cloudinary.com/dodpgohuc/image/upload/v1755058168/7c27f319-d2ae-419b-a9df-a719018e8fe7_na2kbw.jpg",
   ];
 
